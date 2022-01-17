2057d8245e2ef9d653a08fe74d2ece4d28e548d6
Nazar Vinnichuk <nazar@vinnich.uk>
2022-01-17 22:06:51 +0200

    Initialize the project.

---
+++ src/components/app.js
@@ -0,0 +1,5 @@
+export const App = () => {
+    return (
+        <h1>Paper Tactics</h1>
+    );
+};
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import ReactDOM from "react-dom";
+import { App } from "./components/app";
+
+const root = document.getElementById("root");
+
+ReactDOM.render(<App/>, root);
