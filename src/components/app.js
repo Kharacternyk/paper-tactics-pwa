2d778b007d0ff8246ad8983f715f9bfd1100c19a
Nazar Vinnichuk <nazar@vinnich.uk>
2022-01-21 15:59:50 +0200

    Add basic turn indication.

--- src/components/app.js
+++ src/components/app.js
@@ -2,7 +2,11 @@ import { useEffect, useState } from "react"
 import useWebSocket from "react-use-websocket"
 import Button from "react-bootstrap/Button"
 import Stack from "react-bootstrap/Stack"
+import Alert from "react-bootstrap/Alert"
 import { GameMap } from "./game-map"
+import {
+    BsFillLightningFill, BsFillExclamationOctagonFill, BsForwardFill
+} from "react-icons/bs"
 
 const apiUrl = "wss://az7ndrlaxk.execute-api.eu-central-1.amazonaws.com/rolling"
 
@@ -19,9 +23,16 @@ export const App = () => {
     return (
         <Stack style={{padding: "1em"}} gap={3}>
             <Button onClick={() => sendJsonMessage({action: "create-game"})}>
-                Create Game
+                <BsFillLightningFill />Create Game
             </Button>
-            {game && <GameMap game={game} onTurnMade={onTurnMade} />}
+            {game && <>
+                <Alert variant={game.myTurn ? "primary" : "danger"}>
+                    {game.myTurn ? <BsForwardFill /> : <BsFillExclamationOctagonFill />}
+                    {game.myTurn ? " Your turn " : " Opponent's turn "}
+                    ({game.turnsLeft})
+                </Alert>
+                <GameMap game={game} onTurnMade={onTurnMade} />
+            </>}
         </Stack>
     )
 }
