d3b5efcc99ac73658d5d312cfbb549a13f53988b
Nazar Vinnichuk <nazar@vinnich.uk>
2022-01-21 23:34:32 +0200

    Add won/lost notifications.

--- src/components/game.js
+++ src/components/game.js
@@ -7,15 +7,45 @@ const StyledCard = styled(Card)`
     align-self: center;
 `
 
+const getBarProps = game => {
+    if (game.me.reachable.length == 0) {
+        return {
+            label: "You lost!",
+            variant: "danger",
+            now: 100,
+            striped: true,
+            animated: true,
+        }
+    }
+    if (game.opponent.reachable.length == 0) {
+        return {
+            label: "You won!",
+            variant: "success",
+            now: 100,
+            striped: true,
+            animated: true,
+        }
+    }
+    if (game.myTurn) {
+        return {
+            label: "Your turn",
+            variant: "primary",
+            now: game.turnsLeft * 100 / 3,
+        }
+    }
+    return {
+        label: "Opponent's turn",
+        variant: "danger",
+        now: game.turnsLeft * 100 / 3,
+    }
+}
+
+
 export const Game = ({game, onTurnMade}) => {
     return (
         <StyledCard>
             <Card.Header>
-                <ProgressBar
-                    now={game.turnsLeft * 100 / 3} 
-                    label={game.myTurn ? "Your Turn" : "Opponent's Turn"}
-                    variant={game.myTurn ? "primary" : "danger"}
-                />
+                <ProgressBar {...getBarProps(game)} />
             </Card.Header>
             <Card.Body><GameMap game={game} onTurnMade={onTurnMade} /></Card.Body>
             <Card.Footer>Game ID: {game.id}</Card.Footer>
