5172ca8c13da50144efe44f991abd05a5292fbbf
Nazar Vinnichuk <nazar@vinnich.uk>
2022-01-21 15:33:22 +0200

    Further separate <App> and <GameMap>.

--- src/components/app.js
+++ src/components/app.js
@@ -2,9 +2,7 @@ import { useEffect, useState } from "react"
 import useWebSocket from "react-use-websocket"
 import Button from "react-bootstrap/Button"
 import Stack from "react-bootstrap/Stack"
-import Badge from "react-bootstrap/Badge"
 import { GameMap } from "./game-map"
-import { GameMapCell } from "./game-map-cell"
 
 const apiUrl = "wss://az7ndrlaxk.execute-api.eu-central-1.amazonaws.com/rolling"
 
@@ -14,47 +12,8 @@ export const App = () => {
 
     useEffect(() => setGame(lastJsonMessage), [lastJsonMessage])
 
-    let gameView = <Badge bg="warning" text="dark">No current game</Badge>
-
-    if (game) {
-        const cellProps = Array(10).fill().map(() => {
-            return Array(10).fill().map(() => ({}))
-        })
-
-        const setPropsForEach = (array, props, propsFactory) => {
-            array.forEach(([x, y]) => {
-                Object.assign(cellProps[y - 1][x - 1], props)
-                if (propsFactory) {
-                    Object.assign(cellProps[y - 1][x - 1], propsFactory(x, y))
-                }
-            })
-        }
-
-        setPropsForEach(game.opponent.units, { unit: true, opponent: true })
-        setPropsForEach(game.opponent.walls, { wall: true, opponent: true })
-        setPropsForEach(game.opponent.reachable, { reachableByOpponent: true })
-        setPropsForEach(game.me.units, { unit: true, mine: true })
-        setPropsForEach(game.me.walls, { wall: true, mine: true })
-        setPropsForEach(game.me.reachable, { reachableByMe: true }, (x, y) => ({
-            onClick: () => {
-                sendJsonMessage({action: "make-turn", gameId: game.gameId, cell: [x, y]})
-            }
-        }))
-
-        const renderedRows = cellProps.map((row, y) => {
-            const renderedRow = row.map((props, x) => {
-                return <GameMapCell {...props} key={x} />
-            })
-
-            return <tr key={y}>{renderedRow}</tr>
-        })
-
-        gameView = <>
-            <Badge bg="success">Game ID: {game.gameId}</Badge>
-            <GameMap bordered>
-                <tbody>{renderedRows}</tbody>
-            </GameMap>
-        </>
+    const onTurnMade = (x, y) => {
+        sendJsonMessage({action: "make-turn", gameId: game.id, cell: [x, y]})
     }
 
     return (
@@ -62,7 +21,7 @@ export const App = () => {
             <Button onClick={() => sendJsonMessage({action: "create-game"})}>
                 Create Game
             </Button>
-            {gameView}
+            {game && <GameMap game={game} onTurnMade={onTurnMade} />}
         </Stack>
     )
 }
--- src/components/game-map.js
+++ src/components/game-map.js
@@ -1,8 +1,47 @@
 import styled from "styled-components"
 import Table from "react-bootstrap/Table"
+import { GameMapCell } from "./game-map-cell"
 
-export const GameMap = styled(Table)`
-    width: 30em;
-    height: 30em;
+const StyledTable = styled(Table)`
+    width: ${({size}) => 3 * size}em;
+    height: ${({size}) => 3 * size}em;
     align-self: center;
 `
+
+export const GameMap = ({game, onTurnMade}) => {
+    const cellProps = Array(10).fill().map(() => {
+        return Array(10).fill().map(() => ({}))
+    })
+
+    const setPropsForEach = (array, props, propsFactory) => {
+        array.forEach(([x, y]) => {
+            Object.assign(cellProps[y - 1][x - 1], props)
+            if (propsFactory) {
+                Object.assign(cellProps[y - 1][x - 1], propsFactory(x, y))
+            }
+        })
+    }
+
+    setPropsForEach(game.opponent.units, { unit: true, opponent: true })
+    setPropsForEach(game.opponent.walls, { wall: true, opponent: true })
+    setPropsForEach(game.opponent.reachable, { reachableByOpponent: true })
+    setPropsForEach(game.me.units, { unit: true, mine: true })
+    setPropsForEach(game.me.walls, { wall: true, mine: true })
+    setPropsForEach(game.me.reachable, { reachableByMe: true }, (x, y) => ({
+        onClick: () => onTurnMade(x, y)
+    }))
+
+    const renderedRows = cellProps.map((row, y) => {
+        const renderedRow = row.map((props, x) => {
+            return <GameMapCell {...props} key={x} />
+        })
+
+        return <tr key={y}>{renderedRow}</tr>
+    })
+
+    return (
+        <StyledTable bordered size={game.size}>
+            <tbody>{renderedRows}</tbody>
+        </StyledTable>
+    )
+}
