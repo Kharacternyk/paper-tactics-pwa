91b69c99e44291639b30549008ed6104576ff359
Nazar Vinnichuk <nazar@vinnich.uk>
2022-01-24 22:17:05 +0200

    Switch to MUI <AppBar>.

--- src/components/app.js
+++ src/components/app.js
@@ -1,32 +1,33 @@
 import { useState } from "react"
-import Nav from "react-bootstrap/Nav"
-import Navbar from "react-bootstrap/Navbar"
-import Stack from "react-bootstrap/Stack"
-import Container from "react-bootstrap/Container"
 import Alert from "react-bootstrap/Alert"
+import AppBar from "@mui/material/AppBar"
+import Button from "@mui/material/Button"
+import Toolbar from "@mui/material/Toolbar"
+import Typography from "@mui/material/Typography"
 import { Game } from "./game"
 
 const apiUrl = "wss://az7ndrlaxk.execute-api.eu-central-1.amazonaws.com/rolling"
 
 export const App = () => {
-    const [ eventKey, setEventKey ] = useState("how-to")
+    const [ currentPage, setCurrentPage ] = useState("how-to")
 
     return (
-        <Stack gap={3}>
-            <Navbar expand="lg" bg="success" variant="dark">
-                <Container>
-                    <Navbar.Brand>Paper Tactics</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="main-navbar" />
-                    <Navbar.Collapse id="main-navbar">
-                        <Nav defaultActiveKey="how-to" onSelect={setEventKey}>
-                            <Nav.Link eventKey="how-to">How to play</Nav.Link>
-                            <Nav.Link eventKey="new-pvp">Play vs other people</Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-            {eventKey === "how-to" && <Alert variant="success"> Coming soon… </Alert>}
-            {eventKey === "new-pvp" && <Game apiUrl={apiUrl} />}
-        </Stack>
+        <>
+            <AppBar position="sticky">
+                <Toolbar>
+                    <Typography component="h1" sx={{flexGrow: 1}}>
+                        Paper Tactics
+                    </Typography>
+                    <Button color="inherit" onClick={() => setCurrentPage("how-to")}>
+                        Learn to play
+                    </Button>
+                    <Button color="inherit" onClick={() => setCurrentPage("new-pvp")}>
+                        Play vs other players
+                    </Button>
+                </Toolbar>
+            </AppBar>
+            {currentPage === "how-to" && <Alert variant="success"> Coming soon… </Alert>}
+            {currentPage === "new-pvp" && <Game apiUrl={apiUrl} />}
+        </>
     )
 }
