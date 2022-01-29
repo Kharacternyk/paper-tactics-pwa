4b913d346ec0be1ee2206885a8bfe436a6d687b8
Nazar Vinnichuk <nazar@vinnich.uk>
2022-01-29 14:42:26 +0200

    Redesign the turn indicator.

--- src/components/game.js
+++ src/components/game.js
@@ -1,13 +1,11 @@
 import { useEffect, useState } from "react"
 import { GameMap } from "./game-map"
+import { TurnIndicator } from "./turn-indicator"
 import Alert from "@mui/material/Alert"
 import Stack from "@mui/material/Stack"
 import Paper from "@mui/material/Paper"
 import Chip from "@mui/material/Chip"
 import LinearProgress from "@mui/material/LinearProgress"
-import Stepper from "@mui/material/Stepper"
-import Step from "@mui/material/Step"
-import StepLabel from "@mui/material/StepLabel"
 import useWebSocket from "react-use-websocket"
 
 export const Game = ({apiUrl}) => {
@@ -29,17 +27,7 @@ export const Game = ({apiUrl}) => {
     return game ? (
         <Paper elevation={8} sx={{alignSelf: "center"}}>
             <Stack spacing={2} sx={{p: 1}}>
-                <Stepper activeStep={3 - game.turnsLeft}>
-                    <Step key={1}>
-                        <StepLabel />
-                    </Step>
-                    <Step key={2}>
-                        <StepLabel />
-                    </Step>
-                    <Step key={3}>
-                        <StepLabel />
-                    </Step>
-                </Stepper>
+                <TurnIndicator game={game} />
                 <GameMap game={game} onTurnMade={onTurnMade} />
                 <Chip label={`Game ID: ${game.id}`} color="primary"/>
             </Stack>
@@ -48,7 +36,7 @@ export const Game = ({apiUrl}) => {
         <Paper elevation={8} sx={{alignSelf: "center"}}>
             <Alert severity="info">
                 Waiting for someone else to connect…
-                <LinearProgress />
+                <LinearProgress variant="query"/>
             </Alert>
         </Paper>
     )
---
+++ src/components/turn-indicator.js
@@ -0,0 +1,41 @@
+import CircularProgress from "@mui/material/CircularProgress"
+import Alert from "@mui/material/Alert"
+import AlertTitle from "@mui/material/AlertTitle"
+import Box from "@mui/material/Box"
+import Edit from "@mui/icons-material/Edit"
+import EditOff from "@mui/icons-material/EditOff"
+
+export const TurnIndicator = ({game}) => {
+    const color = game.myTurn ? "success" : "error"
+    const icon = game.myTurn ? <Edit color={color} /> : <EditOff color={color} />
+    const message = game.myTurn ? "Your turn!" : "Opponent's turn…"
+    const value = game.turnsLeft * 100 / 3
+
+    const progress = (
+        <Box sx={{position: "relative", display: "inline-flex"}}>
+            <CircularProgress variant="determinate" value={value} color={color} />
+            <Box
+                sx={{
+                    top: 0,
+                    left: 0,
+                    bottom: 0,
+                    right: 0,
+                    position: "absolute",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center"
+                }}
+            >
+                {icon}
+            </Box>
+        </Box>
+    )
+
+    return (
+        <Alert severity={color} icon={progress}>
+            <AlertTitle sx={{display: "flext", alignItems: "center", height: "100%", m: 0}}>
+                {message}
+            </AlertTitle>
+        </Alert>
+    )
+}
