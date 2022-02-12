26a298fdb4384549dcb04e7c755912018f2fda68
Nazar Vinnichuk <nazar@vinnich.uk>
2022-02-12 17:28:40 +0200

    Refresh the icon set.

--- src/components/settings.js
+++ src/components/settings.js
@@ -8,26 +8,22 @@ import AnchorIcon from "@mui/icons-material/Anchor"
 import FlowerIcon from "@mui/icons-material/LocalFlorist"
 import StarsIcon from "@mui/icons-material/AutoAwesome"
 import RocketIcon from "@mui/icons-material/RocketLaunch"
-import AgroIcon from "@mui/icons-material/Agriculture"
+import BeerIcon from "@mui/icons-material/SportsBar"
 import BakeryIcon from "@mui/icons-material/BakeryDining"
-import ForestIcon from "@mui/icons-material/Forest"
+import BoltIcon from "@mui/icons-material/Bolt"
 import BulbIcon from "@mui/icons-material/EmojiObjects"
-import BugIcon from "@mui/icons-material/BugReport"
-import FortIcon from "@mui/icons-material/Fort"
 
 export const icons = [
     <CrossIcon />,
+    <BoltIcon />,
     <FlakeIcon />,
     <AnchorIcon />,
     <FlowerIcon />,
+    <BulbIcon />,
     <StarsIcon />,
     <RocketIcon />,
-    <AgroIcon />,
+    <BeerIcon />,
     <BakeryIcon />,
-    <ForestIcon />,
-    <BulbIcon />,
-    <BugIcon />,
-    <FortIcon />
 ]
 
 export const Settings = ({onIconChanged, iconIndex}) => {
