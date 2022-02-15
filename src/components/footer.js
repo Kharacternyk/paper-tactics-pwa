84e74a9de37ed8db894997e6ffcd2f9f9e12cd59
Nazar Vinnichuk <nazar@vinnich.uk>
2022-02-15 19:22:54 +0200

    Provide a saythanks.io link in the footer.

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,21 +1,19 @@
 import {BadgeAlert} from "./badge-alert"
 import Link from "@mui/material/Link"
-import GithubIcon from "@mui/icons-material/GitHub"
+import ThanksIcon from "@mui/icons-material/SentimentSatisfiedAlt"
 
 export const Footer = () => {
     return (
         <BadgeAlert
-            subtitle={<>
-                {"Check it out "}
-                <Link href="https://github.com/Kharacternyk/paper-tactics">
-                    on GitHub
-                </Link>
-            </>}
+            subtitle="It will make my day!"
             color="primary"
             progress={100}
-            icon={<GithubIcon color="primary"/>}
+            icon={<ThanksIcon color="primary"/>}
         >
-            This website is open source
+            You can
+            {" "}<Link href="https://saythanks.io/to/Kharacternyk">say thanks</Link>{" "}
+            to the
+            {" "}<Link href="https://vinnich.uk">sole developer</Link>
         </BadgeAlert>
     )
 }
