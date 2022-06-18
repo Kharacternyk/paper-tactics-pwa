fd7fc8ce2b5bcfb3133142925b81b04a6cd64cef
Nazar Vinnichuk <nazar@vinnich.uk>
2022-06-18 17:22:22 +0300

    Remember the last used tab.

--- src/components/app.js
+++ src/components/app.js
@@ -2,27 +2,27 @@ import {Tutorial} from "./tutorial"
 import {GameLobby} from "./game-lobby"
 import {Header} from "./header"
 import {Footer} from "./footer"
-import {useState} from "react"
 import {ThemeProvider, createTheme} from "@mui/material/styles"
 import primary from "@mui/material/colors/teal"
 import secondary from "@mui/material/colors/red"
 import Alert from "@mui/material/Alert"
 import Stack from "@mui/material/Stack"
 import CssBaseline from "@mui/material/CssBaseline"
+import useCookie from "react-use-cookie"
 
 export const App = () => {
-    const [currentPage, setCurrentPage] = useState(0)
+    const [currentPage, setCurrentPage] = useCookie("tab", 0)
 
     return (
         <ThemeProvider theme={theme}>
             <CssBaseline />
             <Header
-                value={currentPage}
+                value={Number(currentPage)}
                 onChange={(event, page) => setCurrentPage(page)}
             />
             <Stack gap={2} alignItems="center" px={2} py={2}>
-                {currentPage === 0 && <Tutorial />}
-                {currentPage === 1 && <GameLobby />}
+                {Number(currentPage) === 0 && <Tutorial />}
+                {Number(currentPage) === 1 && <GameLobby />}
                 <Footer />
             </Stack>
         </ThemeProvider>
