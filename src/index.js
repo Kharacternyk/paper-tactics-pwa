74f47ffc180c76d63995905f0b41958c99615fba
Nazar Vinnichuk <nazar@vinnich.uk>
2022-06-19 13:59:24 +0300

    Log the commit that has been built.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,15 @@ import "@fontsource/roboto/latin.css"
 import "@fontsource/ibm-plex-mono/latin-500-italic.css"
 import ReactDOM from "react-dom"
 import {App} from "./components/app"
+import fs from "fs"
+import path from "path"
+
+const version = fs.readFileSync(
+    path.join(__dirname, "/../.git/refs/heads/master"),
+    "utf8"
+)
+
+console.log(`PWA Version: ${version}`)
 
 const root = document.getElementById("root")
 
