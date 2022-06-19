3ee4a3df64b20c9c250e3c301b5ea6c571486b2b
Nazar Vinnichuk <nazar@vinnich.uk>
2022-06-19 18:22:42 +0300

    Add a sample game to the tutorial.

--- src/components/tutorial.js
+++ src/components/tutorial.js
@@ -1,6 +1,9 @@
 import {Section} from "./section"
+import {GameMap} from "./game-map"
 import Link from "@mui/material/Link"
 import Typography from "@mui/material/Typography"
+import MyIcon from "@mui/icons-material/Close"
+import OpponentIcon from "@mui/icons-material/FiberManualRecordOutlined"
 
 export const Tutorial = () => {
     return (
@@ -10,12 +13,10 @@ export const Tutorial = () => {
                 on a square grid, usually 10×10. The goal of the game is to
                 outlive your opponent, i.e., a player that cannot make a turn is
                 considered defeated. Each turn consists of moves, usually three.
-                On your move, you must place either a unit (looks like a funny
-                icon, e.g., the X in the upper left corner of the logo) or a
-                wall (looks like a filled opponent's icon, e.g., the circle in
-                the lower left corner of the logo). Units are placed only on
-                empty cells, while walls are placed only on opponent's units.
-                You cannot place anything on walls or your own units.
+                On your move, you must place either a unit or a wall. Units are
+                placed only on empty cells, while walls are placed only on
+                opponent's units. You cannot place anything on walls or your own
+                units.
             </Paragraph>
             <Paragraph>
                 Units and walls can only be placed on cells that are reachable
@@ -25,6 +26,38 @@ export const Tutorial = () => {
                 that connects the cell and a player's unit. Two cells are
                 considered adjacent if they share a side or a corner.
             </Paragraph>
+            <Paragraph>
+                The sample below showcases how a game looks like:
+                <MyIcon sx={{verticalAlign: "bottom"}} />
+                and
+                <OpponentIcon sx={{verticalAlign: "bottom"}} />
+                are your and opponent's units, respectively.{" "}
+                <MyIcon
+                    sx={{
+                        verticalAlign: "bottom",
+                        bgcolor: "black",
+                        color: "white",
+                    }}
+                />{" "}
+                and{" "}
+                <OpponentIcon
+                    sx={{
+                        verticalAlign: "bottom",
+                        bgcolor: "black",
+                        color: "white",
+                    }}
+                />{" "}
+                are opponent's and your walls, respectively. Cells with a white
+                background are reachable for you, while the ones with a gray
+                background are not.
+            </Paragraph>
+            <Section>
+                <GameMap
+                    game={exampleGame}
+                    icons={gameIcons}
+                    gamePreferences={{size: 4}}
+                />
+            </Section>
             <Paragraph>
                 In the <b>«Play»</b> tab, you can choose a game mode, the count
                 of moves per turn, the size of the grid, and an icon for your
@@ -60,3 +93,45 @@ const Paragraph = ({children}) => (
         </Typography>
     </Section>
 )
+
+const gameIcons = {
+    me: <MyIcon />,
+    opponent: <OpponentIcon />,
+}
+
+const exampleGame = {
+    me: {
+        units: [
+            [2, 1],
+            [4, 1],
+            [1, 1],
+        ],
+        walls: [
+            [1, 2],
+            [1, 3],
+        ],
+        reachable: [
+            [2, 2],
+            [1, 3],
+            [2, 3],
+            [1, 4],
+            [2, 4],
+            [1, 1],
+            [3, 1],
+            [3, 2],
+            [4, 2],
+        ],
+    },
+    opponent: {
+        units: [
+            [2, 4],
+            [3, 4],
+            [4, 4],
+        ],
+        walls: [
+            [3, 2],
+            [4, 3],
+        ],
+        reachable: [],
+    },
+}
