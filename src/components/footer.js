a622b4a1409217d5ed3cd194b2ab85a3510afbfc
Nazar Vinnichuk <nazar@vinnich.uk>
2023-11-29 16:17:00 +0200

    Add a section for the sponsor.

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,16 +1,35 @@
-import {BadgeAlert} from "./badge-alert"
-import Link from "@mui/material/Link"
 import ContactIcon from "@mui/icons-material/Mail"
+import Link from "@mui/material/Link"
+import SvgIcon from "@mui/material/SvgIcon"
+import SponsorLogo from "../logo-sponsor.svg"
+import {BadgeAlert} from "./badge-alert"
 
 export const Footer = () => {
+    const sponsorLogo = <SvgIcon inheritViewBox component={SponsorLogo} />
+
     return (
-        <BadgeAlert
-            subtitle="Bug reports, feature requests, or just thanks"
-            color="primary"
-            progress={100}
-            icon={<ContactIcon color="primary" />}
-        >
-            You can <Link href="https://vinnich.uk">contact the developer</Link>{" "}
-        </BadgeAlert>
+        <>
+            <BadgeAlert
+                subtitle="Bug reports, feature requests, or just thanks"
+                color="primary"
+                progress={100}
+                icon={<ContactIcon color="primary" />}
+            >
+                You can{" "}
+                <Link href="https://vinnich.uk">contact the developer</Link>
+            </BadgeAlert>
+            <BadgeAlert
+                subtitle={
+                    "Keep track of your digital life by visualizing " +
+                    "connections between your online accounts, devices and other assets"
+                }
+                color="primary"
+                progress={100}
+                icon={sponsorLogo}
+            >
+                Paper Tactics is sponsored by{" "}
+                <Link href="https://depauth.com">DepAuth</Link>
+            </BadgeAlert>
+        </>
     )
 }
