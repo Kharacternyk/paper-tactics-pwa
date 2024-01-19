1a7b57861402a47aa90a3e07684989d8edd61ffb
Nazar Vinnichuk <nazar@vinnich.uk>
2024-01-19 19:35:39 +0200

    Add a link to the Discord server.

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,17 +1,23 @@
 import ContactIcon from "@mui/icons-material/Mail"
 import Link from "@mui/material/Link"
 import SvgIcon from "@mui/material/SvgIcon"
+import DiscordLogo from "../logo-discord.svg"
 import SponsorLogo from "../logo-sponsor.svg"
 import {BadgeAlert} from "./badge-alert"
 
 export const Footer = () => {
     const sponsorLogo = <SvgIcon inheritViewBox component={SponsorLogo} />
+    const discordLogo = <SvgIcon inheritViewBox component={DiscordLogo} />
 
     return (
         <>
-            <BadgeAlert color="primary" progress={100} icon={sponsorLogo}>
-                Paper Tactics is sponsored by&nbsp;
-                <Link href="https://depauth.com">DepAuth</Link>
+            <BadgeAlert color="primary" progress={100} icon={discordLogo}>
+                <div>
+                    You can{" "}
+                    <Link href="https://discord.gg/c6X7tDTKSX">
+                        discuss the game on Discord
+                    </Link>
+                </div>
             </BadgeAlert>
             <BadgeAlert
                 subtitle="Bug reports, feature requests, or just thanks"
@@ -21,6 +27,12 @@ export const Footer = () => {
             >
                 You can{" "}
                 <Link href="https://vinnich.uk">contact the developer</Link>
+            </BadgeAlert>
+            <BadgeAlert color="primary" progress={100} icon={sponsorLogo}>
+                <div>
+                    Paper Tactics is sponsored by{" "}
+                    <Link href="https://depauth.com">DepAuth</Link>
+                </div>
             </BadgeAlert>
         </>
     )
