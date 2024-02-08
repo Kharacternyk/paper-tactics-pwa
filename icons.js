b7c687345884e891f5be8135acf53da4df38311e
Nazar Vinnichuk <nazar@vinnich.uk>
2024-02-08 20:52:22 +0200

    Add more icons.

--- icons.js
+++ icons.js
@@ -17,16 +17,20 @@ import Cup from "@mui/icons-material/EmojiEventsOutlined"
 import Bulb from "@mui/icons-material/EmojiObjectsOutlined"
 import Heart from "@mui/icons-material/FavoriteBorderOutlined"
 import Circle from "@mui/icons-material/FiberManualRecordOutlined"
+import FireTruck from "@mui/icons-material/FireTruckOutlined"
 import Flag from "@mui/icons-material/FlagOutlined"
 import Star from "@mui/icons-material/GradeOutlined"
 import HealthAndSafety from "@mui/icons-material/HealthAndSafetyOutlined"
 import Hive from "@mui/icons-material/HiveOutlined"
+import Fire from "@mui/icons-material/LocalFireDepartmentOutlined"
 import Cart from "@mui/icons-material/LocalGroceryStoreOutlined"
 import LunchDining from "@mui/icons-material/LunchDiningOutlined"
+import ComingSoon from "@mui/icons-material/MinimizeOutlined"
 import Movie from "@mui/icons-material/MovieOutlined"
 import Music from "@mui/icons-material/MusicNoteOutlined"
 import Moon from "@mui/icons-material/NightsStayOutlined"
 import Bell from "@mui/icons-material/NotificationsNoneOutlined"
+import PushPin from "@mui/icons-material/PushPinOutlined"
 import Rocket from "@mui/icons-material/RocketLaunchOutlined"
 import Science from "@mui/icons-material/ScienceOutlined"
 import Soup from "@mui/icons-material/SoupKitchenOutlined"
@@ -39,6 +43,9 @@ import Check from "@mui/icons-material/VerifiedOutlined"
 import SvgIcon from "@mui/material/SvgIcon"
 import Hat from "./images/hat.svg"
 import Pawn from "./images/pawn.svg"
+import Pumpkin from "./images/pumpkin.svg"
+import Snowglobe from "./images/snowglobe.svg"
+import Zeppelin from "./images/zeppelin.svg"
 
 export const icons = [
     <Cross />,
@@ -81,4 +88,14 @@ export const icons = [
     <Movie />,
     <Chair />,
     <Soup />,
+    <SvgIcon inheritViewBox component={Zeppelin} />,
+    <SvgIcon inheritViewBox component={Pumpkin} />,
+    <SvgIcon inheritViewBox component={Snowglobe} />,
+    <Fire />,
+    <FireTruck />,
+    <PushPin />,
+    <ComingSoon />,
+    <ComingSoon />,
+    <ComingSoon />,
+    <ComingSoon />,
 ]
